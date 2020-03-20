a9fc803cfb21f0f50aed6aacc35bc95c8ef51aea
Conrad Eggan <conrade@redcatmfg.com>
2020-03-20 12:09:14 -0500

    interpolate function

--- src/kat_zapper.ino
+++ src/kat_zapper.ino
@@ -16,6 +16,7 @@ void setup() {
   Particle.function("pan", pan);
   Particle.function("tilt", tilt);
   Particle.function("exercise", exercise);
+  Particle.function("interpolate", interpolate);
   Particle.function("speed", setSpeed);
 
   panServo.attach(D3);
@@ -24,7 +25,8 @@ void setup() {
   panServo.write(0);  // tell servo to go to a particular angle
   delay(500);
   
-  panServo.write(90);              
+  panServo.write(100);  
+  tiltServo.write(135);            
   delay(500); 
 }
 
@@ -35,9 +37,14 @@ void loop() {
 }
 
 int pan(String pan) {
-  int setPoint = pan.toInt();
-  panServo.write(setPoint);
-  return setPoint;
+  int panStart = panServo.read();
+  int panEnd = pan.toInt();
+  int z = (panEnd >= panStart) ? 1 : -1;
+  for (int i=panStart + z; i != panEnd; i += z) {
+    panServo.write(i);
+    delay(servoDelay);
+  }
+  return panEnd;
 }
 
 int tilt(String tilt) {
@@ -56,14 +63,78 @@ int exercise(String go) {
     delay(1000);
     panServo.write(100);
     tiltServo.write(135);
-    return servoDelay;
+    return 1;
   }
   else {
     return 0;
   }
 }
 
+int interpolate(String endpoint) {
+  int pEnd = endpoint.substring(0, endpoint.indexOf(",")).toInt();
+  int tEnd = endpoint.substring(endpoint.indexOf(",") + 1).toInt();
+  int pStart = panServo.read();
+  int tStart = tiltServo.read();
+  float ptRatio = 0;
+  
+  // Pan only move
+  if (abs(pStart - pEnd) > 0 && abs(tStart - tEnd) == 0) {
+    int z = (pEnd >= pStart) ? 1 : -1;
+    for (int i=pStart + z; i != pEnd; i += z) {
+      panServo.write(i);
+      delay(servoDelay);
+    }
+  }
+  // Tilt only move
+  if (abs(tStart - tEnd) > 0 && abs(pStart - pEnd) == 0) {
+    int z = (tEnd >= tStart) ? 1 : -1;
+    for (int i=tStart + z; i != tEnd; i += z) {
+      tiltServo.write(i);
+      delay(servoDelay);
+    }
+  }
+  // Pan & Tilt move
+  if (abs(pStart - pEnd) > 0 && abs(tStart - tEnd) > 0) {
+    ptRatio = ((float)abs(pStart - pEnd) / (float)abs(tStart - tEnd));
+    // pan move larger than or same as tilt
+    if (ptRatio >= 1) {
+      int pz = (pEnd >= pStart) ? 1 : -1;
+      float tz = ((tEnd >= tStart) ? 1 : -1) / ptRatio;
+      float tNext = tStart + tz;
+      for (int i=pStart + pz; i != pEnd; i += pz) {
+        panServo.write(i);
+        tiltServo.write(int(tNext));
+        tNext += tz;
+        delay(servoDelay);
+      }
+    }
+    // tilt move larger than pan
+    if (ptRatio < 1) {
+      int tz = (tEnd >= tStart) ? 1 : -1;
+      float pz = ((pEnd >= pStart) ? 1 : -1) * ptRatio;
+      float pNext = pStart + pz;
+      for (int i=tStart + tz; i != tEnd; i += tz) {
+        tiltServo.write(i);
+        panServo.write(int(pNext));
+        pNext += pz;
+        delay(servoDelay);
+      }
+    }
+  }
+
+
+  Particle.publish("info", String::format("pStart=%d, pEnd=%d, tStart=%d, tEnd=%d, ptRatio=%4f", pStart, pEnd, tStart, tEnd, ptRatio));
+  return 1;
+}
+
 int setSpeed(String speed) {
-  servoDelay = map(speed.toInt(), 1, 10, 100, 15);
+  int s = speed.toInt();
+  if (s >= 1 && s <= 10) {
+    servoDelay = map(speed.toInt(), 1, 10, 60, 3);
+    Particle.publish("speed", String::format("Speed set to %d (%d delay)", s, servoDelay));
+  }
+  else {
+    Particle.publish("error", "Speed setting must be in the range of 1 - 10.");
+  }
   return servoDelay;
 }
